7f193b0abeed4a91cac300979cee22b972d6a95e
TriPOLOSKI <145340240+sudoC7@users.noreply.github.com>
2024-03-16 19:25:28 +0100

    Update RECETTES1.sql

--- RECETTES/RECETTES1.sql
+++ RECETTES/RECETTES1.sql
@@ -1,48 +1,34 @@
 CREATE TABLE INGREDIENTS( -- yes
-   id_ingredients INT AUTO_INCREMENT,
+   id_ingredients INTEGER PRIMARY KEY AUTOINCREMENT,
    NomIngredient TEXT NOT NULL,
    UniteMesure VARCHAR(50) DEFAULT NULL,
-   Prix INT NOT NULL,
-   PRIMARY KEY(id_ingredients)
+   Prix INT NOT NULL
 );
 CREATE TABLE CATEGORIE( -- yes
-   id_categorie INT AUTO_INCREMENT,
-   NomCategorie VARCHAR(50) DEFAULT NULL,
-   PRIMARY KEY(id_categorie)
+   id_categorie INTEGER PRIMARY KEY AUTOINCREMENT,
+   NomCategorie VARCHAR(50) DEFAULT NULL
 );
-CREATE TABLE RECETTE( -- yes 
-   id_recette INT AUTO_INCREMENT,
+CREATE TABLE RECETTE ( -- yes 
+   id_recette INTEGER PRIMARY KEY AUTOINCREMENT,
    Nom VARCHAR(50) NOT NULL,
    Instructions TEXT NOT NULL,
    TempPreparation INT NOT NULL,
-   id_categorie INT DEFAULT NULL,    
-   PRIMARY KEY(id_recette),
+   id_categorie INT,    
    FOREIGN KEY(id_categorie) REFERENCES CATEGORIE(id_categorie)
 );
 CREATE TABLE PREPARATION( -- yes 
-   id_recette INT,
-   id_ingredients INT,
+   id_recette INTEGER,
+   id_ingredients INTEGER,
    quantite INT NOT NULL,
    PRIMARY KEY(id_recette, id_ingredients),
    FOREIGN KEY(id_recette) REFERENCES RECETTE(id_recette),
    FOREIGN KEY(id_ingredients) REFERENCES INGREDIENTS(id_ingredients)
 );
 
-
-SELECT * 
-FROM CATEGORIE;
-
-SELECT * 
-FROM RECETTE;
-
-SELECT * 
-FROM PREPARATION;
-
-SELECT * 
-FROM INGREDIENTS;
-
--- suprimmer la table exemple
---DROP TABLE --table;
+DROP TABLE PREPARATION;
+DROP TABLE RECETTE;
+DROP TABLE CATEGORIE;
+DROP TABLE INGREDIENTS;
 
 
 INSERT INTO CATEGORIE (NomCategorie) VALUES ("Plat"),("Plat"),("Plat"),("Plat"),("Entree"),("Plat"),("Dessert"),("Plat"),("Plat"),("Dessert");
@@ -152,6 +138,16 @@ INSERT INTO INGREDIENTS (NomIngredient, UniteMesure, Prix) VALUES
     - Cacao en poudre", "gr", 11);
 
 
+SELECT * 
+FROM CATEGORIE;
 
+SELECT * 
+FROM RECETTE;
+
+SELECT * 
+FROM PREPARATION;
+
+SELECT * 
+FROM INGREDIENTS;
 
 
